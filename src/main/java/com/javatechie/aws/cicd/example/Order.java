1395ce6079be9b4a7e921aa3ff581fb52a614c66
Victordng <89790931+Victordng86@users.noreply.github.com>
2023-10-18 19:07:06 +0200

    first commit

---
+++ src/main/java/com/javatechie/aws/cicd/example/Order.java
@@ -0,0 +1,16 @@
+package com.javatechie.aws.cicd.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+}
